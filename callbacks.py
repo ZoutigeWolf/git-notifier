a186e482a0baaacd6e2e6863230b0c37732ea723
ZoutigeWolf <guuskamphuis@gmail.com>
2023-11-01 12:54:23 +0100

    fix

--- callbacks.py
+++ callbacks.py
@@ -8,7 +8,7 @@ config = load_json("config.json")
 
 def restart_viscon_support_app() -> None:
     git_pull("/home/zouti/VisconSupportApp")
-    npm_install("/home/zouti/VisconSupportApp")
+
     restart_service("viscon-support-app")
 
     send_notification("Git Notifier", "Restarted VisconSupportApp")
@@ -36,16 +36,6 @@ def git_pull(directory: str) -> None:
 def restart_service(service: str) -> int:
     return subprocess.call(["sh", "restart.sh", config["sudo_password"], service])
 
-
-def npm_install(directory: str) -> None:
-    wd = os.getcwd()
-
-    os.chdir(directory)
-
-    p = subprocess.Popen(["npm", "install"])
-    p.wait()
-
-    os.chdir(wd)
 
 def send_notification(message: str, description: str):
     requests.post('https://api.mynotifier.app', {
